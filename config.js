3d86d1bb1bcb1effc06b3e5736b4a23981180d6e
Marko Trapani <mjtrapani@gmail.com>
2018-11-25 20:28:25 -0800

    config changes

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ config.db = {};
 config.db.type = 'mysql';
 config.db.charset = 'utf8';
 
-config.db.username = 'root';
+config.db.username = 'mtrapani';
 config.db.password = 'admin123';
-config.db.host = 'localhost';
-config.db.dbname = 'abd'; // DB name
+config.db.host = 'dbserver.engr.scu.edu';
+config.db.dbname = 'sdb_mtrapani'; // DB name
 
 config.db.listings_tbl = 'listings'; // table name
 // config.db.another_tbl = 'next_table'; // ...
